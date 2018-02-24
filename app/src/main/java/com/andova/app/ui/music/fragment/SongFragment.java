8937de90395afb28d6ce5276e25853c570333ed7
Zsago <3427241441@qq.com>
2018-02-24 11:27:57 +0800

    优化分割线样式

--- app/src/main/java/com/andova/app/ui/music/fragment/SongFragment.java
+++ app/src/main/java/com/andova/app/ui/music/fragment/SongFragment.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -15,6 +14,7 @@ import android.view.ViewGroup;
 import com.andova.app.Constants;
 import com.andova.app.R;
 import com.andova.app.ui.music.adapter.SongListAdapter;
+import com.andova.app.ui.music.decoration.DividerDecoration;
 import com.andova.app.ui.music.loader.SongLoader;
 import com.andova.app.ui.music.model.Song;
 
@@ -84,7 +84,7 @@ public class SongFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.rv_song);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setAdapter(mAdapter);
-        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        recyclerView.addItemDecoration(new DividerDecoration(getActivity(), LinearLayoutManager.VERTICAL, true));
 
         loadSongs();
     }

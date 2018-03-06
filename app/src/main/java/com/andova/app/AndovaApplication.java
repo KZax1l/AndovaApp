0f4b5cb4382b39253b0a176db2518ba70a3c4f9b
Zsago <3427241441@qq.com>
2018-03-06 15:05:17 +0800

    集成内存泄漏检测工具

--- app/src/main/java/com/andova/app/AndovaApplication.java
+++ app/src/main/java/com/andova/app/AndovaApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 /**
  * Created by Administrator on 2018-02-28.
@@ -12,14 +14,34 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
  * @since 1.0.0
  */
 public class AndovaApplication extends Application {
+    private RefWatcher refWatcher;
+
     @Override
     public void onCreate() {
         super.onCreate();
         initImageLoader();
+        refWatcher = setupLeakCanary();
     }
 
     private void initImageLoader() {
         ImageLoaderConfiguration localImageLoaderConfiguration = new ImageLoaderConfiguration.Builder(this).build();
         ImageLoader.getInstance().init(localImageLoaderConfiguration);
+    }
+
+    private RefWatcher setupLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return RefWatcher.DISABLED;
+        }
+        return LeakCanary.install(this);
+    }
+
+    /**
+     * 调用{@link RefWatcher#watch(Object)}来监听可能内存泄漏的对象（如：Fragment对象）
+     * <p>
+     * LeakCanary在调用{@link LeakCanary#install(Application)}方法时会启动一个ActivityRefWatcher类，
+     * 它用于自动监控Activity执行onDestroy方法之后是否发生内存泄露
+     */
+    public RefWatcher getRefWatcher() {
+        return refWatcher;
     }
 }
